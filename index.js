12f2f3cf005e3824db47655d4cabae56a17f274b
Christopher Caraway <chrisccaraway@gmail.com>
2017-11-06 04:08:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
+var dwarves = ["Dopey", "Grumpy", "Bashful"];
+
 function dwarfRollCall(dwarves) {
+	for(var i =0; i < dwarves.length; i++) {
+		return (i+1) + '. ' + dwarves[i] + ' ' + (i+2) + '. '+ dwarves[i+1]
+		+ ' ' + (i+3) + '. ' + dwarves[i+2] + ' ';
+	}
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls) {
+
+  var planeteerList = [];
+  for (var i = 0; i < planeteerCalls.length; i++) {
+    planeteerList.push(`${planeteerCalls[i]}! `)
+  }
+  var allCaps = planeteerList.map(function(item){
+    return item.toUpperCase();
+  })
+  return allCaps;
 }
 
 function longPlaneteerCalls(words) {
+	for (var i = 0; i < words.length; i++) {
+		if(words[i].length > 4) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 }
 
 function findTheCheese (foods) {
+	for(var i =0; i < foods.length; i ++) {
+		if(foods[i] === 'cheddar') {
+			return foods[i];
+		}
+	}
+	if(foods !== 'cheddar') {
+		return 'no cheese!'
+	}
 }
